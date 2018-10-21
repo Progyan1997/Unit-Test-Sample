7dfc699a985733ddafc359869b8d2809e5e1e05f
Progyan Bhattacharya <progyanb@acm.org>
2018-10-21 16:39:48 +0530

    Calculator: Single Line Method for Each Arithmetic Operation

---
+++ Single-Line-Methods/Calculator.test.js
@@ -0,0 +1,8 @@
+const { add, subtract, multiply, divide } = require('./Calculator');
+
+// Write Your Test Cases Here
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+//...
