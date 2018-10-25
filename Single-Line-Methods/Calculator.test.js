72f379d845321824ade9b9fbe78def5339a7a0a4
Sumit Kumar Mridha <3sumit5@gmail.com>
2018-10-25 20:37:29 +0530

    test cases added where the functions are going to fail

--- Single-Line-Methods/Calculator.test.js
+++ Single-Line-Methods/Calculator.test.js
@@ -1,8 +1,18 @@
 const { add, subtract, multiply, divide } = require('./Calculator');
 
 // Write Your Test Cases Here
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+test('Good + Morning should be 0', () => {
+    expect(add("Good", "Morning")).toBe(0);
 });
 
-//...
+test('Good - Morning should be 0', () => {
+    expect(subtract("Good", "Morning")).toBe(0);
+});
+
+test('Good * Morning should be 0', () => {
+    expect(multiply("Good", "Morning")).toBe(0);
+});
+
+test('Good / Morning should be 0', () => {
+    expect(divide("Good", "Morning")).toBe(0);
+});
--- Single-Line-Methods/ExtendedCalculator.test.js
+++ Single-Line-Methods/ExtendedCalculator.test.js
@@ -1,3 +1,19 @@
 // Import methods from ExtendedCalculator
+const { factorial, fibonacci, power, reciprocal } = require('./ExtendedCalculator');
 
 // Enter Your Tests Here
+test('factorial of 5 is 120', () => {
+    expect(factorial(5)).toBe(120);
+});
+
+test('fibonacci(9) should be 34', () => {
+    expect(fibonacci(9)).toBe(34);
+});
+
+test('10^100 means 100 zeros after 1', () => {
+    expect(power(10,100)).toBe(1e100);
+});
+
+test('inverse of 7 is 0.142857', () => {
+    expect(reciprocal(7)).toBe(0.142857);
+});
