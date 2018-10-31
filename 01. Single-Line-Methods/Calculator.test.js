e83c9f2fb4c3d3ddabfd6da01fce187200d5c5b3
Amrita Singha Roy <amrito1996@gmail.com>
2018-10-31 19:09:29 +0530

    Calculator.test: Add test cases for add function
    testing wrong data-type, addition of positive number and negetive data-type.
    
    Signed-off-by: Amrita Singha Roy <amrito1996@gmail.com>

--- 01. Single-Line-Methods/Calculator.test.js
+++ 01. Single-Line-Methods/Calculator.test.js
@@ -1,8 +1,16 @@
 const { add, subtract, multiply, divide } = require('./Calculator');
 
 // Write Your Test Cases Here
-test('Good + Morning should be 0', () => {
-    expect(add("Good", "Morning")).toBe(0);
+test('Good + Morning should be NaN', () => {
+    expect(isNaN(add("Good", "Morning"))).toBe(true);
+});
+
+test('5 + 10 should be 15', () => {
+    expect(add(5, 10)).toBe(15);
+});
+
+test('-10 + 5 should be -5', () => {
+    expect(add(-10, 5)).toBe(-5);
 });
 
 test('Good - Morning should be 0', () => {
