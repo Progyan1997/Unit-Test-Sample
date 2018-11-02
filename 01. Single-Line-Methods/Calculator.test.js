c6deea2624fe57585458643a635caef161f58a60
Amrita Singha Roy <amrito1996@gmail.com>
2018-11-02 20:15:24 +0530

    Calculator.test: added test cases for addition, subtraction, multiplication and division function
    testing wrong data-type, subtraction, multiplication and division of positive number and negetive data-type.
    
    Signed-off-by: Amrita Singha Roy <amrito1996@gmail.com>

--- 01. Single-Line-Methods/Calculator.test.js
+++ 01. Single-Line-Methods/Calculator.test.js
@@ -13,14 +13,90 @@ test('-10 + 5 should be -5', () => {
     expect(add(-10, 5)).toBe(-5);
 });
 
-test('Good - Morning should be 0', () => {
-    expect(subtract("Good", "Morning")).toBe(0);
+test('4 + 9 should be 13', () => {
+    expect(add(4,9)).toBe(13);
 });
 
-test('Good * Morning should be 0', () => {
-    expect(multiply("Good", "Morning")).toBe(0);
+test('0 + 0 should be 0', () => {
+    expect(add(0,0)).toBe(0);
 });
 
-test('Good / Morning should be 0', () => {
-    expect(divide("Good", "Morning")).toBe(0);
+test('Good - Morning should be NaN', () => {
+    expect(isNaN(subtract("Good", "Morning"))).toBe(true);
+});
+
+test('10 - 5 should be 5', () => {
+    expect(subtract(10,5)).toBe(5);
+});
+
+test('-10 - 5 should be -15',() => {
+    expect(subtract(-10,5)).toBe(-15);
+});
+
+test('8 - 17 should be -9', () => {
+    expect(subtract(8,17)).toBe(-9);
+});
+
+test('0 - 0 should be 0', () => {
+    expect(subtract(0,0)).toBe(0);
+});
+
+test('1 - 0 should be 1', () => {
+    expect(subtract(1,0)).toBe(1);
+});
+
+test('Good * Morning should be NaN', () => {
+    expect(isNaN(multiply("Good", "Morning"))).toBe(true);
+});
+
+test('8 * 5 should be 40', () => {
+    expect(multiply(8,5)).toBe(40);
+});
+
+test('(-7) * 5 should be -35',() => {
+    expect(multiply(-7,5)).toBe(-35);
+});
+
+test('(-7) * (-5) should be 35', () => {
+    expect(multiply(-7,-5)).toBe(35);
+});
+
+test('0 * 0 should be 0', () => {
+    expect(multiply(0,0)).toBe(0);
+});
+
+test('1 * 0 should be 0', () => {
+    expect(multiply(1,0)).toBe(0);
+});
+
+test('Good / Morning should be NaN', () => {
+    expect(isNaN(divide("Good", "Morning"))).toBe(true);
+});
+
+test('10 / 5 should be 2', () => {
+    expect(divide(10,5)).toBe(2);
+});
+
+test('(-95) / 5 should be -19',() => {
+    expect(divide(-95,5)).toBe(-19);
+});
+
+test('(-12) * (-6) should be 2', () => {
+    expect(divide(-12,-6)).toBe(2);
+});
+
+test('1 / 1 should be 1', () => {
+    expect(divide(1,1)).toBe(1);
+});
+
+test('0 / 1 should be 0', () => {
+    expect(divide(0,1)).toBe(0);
+});
+
+test('0 / 0 should be NaN', () => {
+    expect(isNaN(divide(0,0))).toBe(true);
+});
+
+test('1 / 0 should be NaN', () => {
+    expect(isNaN(divide(1,0))).toBe(true);
 });
